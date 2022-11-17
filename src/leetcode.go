bf5ed587fd6f5f913aa7b047fc38e6cbf5ac6215
Neekity <tc_huang1995@163.com>
2022-11-17 20:07:14 +0800

    binary search

--- src/leetcode.go
+++ src/leetcode.go
@@ -12,3 +12,21 @@ func TwoSum(nums []int, target int) []int {
 	}
 	return []int{0, 0}
 }
+
+func BinarySearch(nums []int, target int) int {
+	l, r, mid := 0, len(nums)-1, 0
+	for l < r {
+		mid = l + (r-l)/2
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			return mid
+		}
+	}
+	if nums[l] == target {
+		return l
+	}
+	return -1
+}
