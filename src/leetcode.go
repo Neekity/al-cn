2f8e0d48348c858273a68cfc92452727ee0c82b4
Neekity <tc_huang1995@163.com>
2022-12-12 17:45:18 +0800

    MergeTwoLists

--- src/leetcode.go
+++ src/leetcode.go
@@ -82,3 +82,26 @@ func ReverseList(head *common.ListNode) *common.ListNode {
 	}
 	return prev
 }
+
+func MergeTwoLists(list1 *common.ListNode, list2 *common.ListNode) *common.ListNode {
+	dump := &common.ListNode{-1, nil}
+	cur := dump
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			cur.Next = list1
+			cur, list1 = list1, list1.Next
+			cur.Next = nil
+		} else if list1.Val > list2.Val {
+			cur.Next = list2
+			cur, list2 = list2, list2.Next
+			cur.Next = nil
+		}
+	}
+	if list1 == nil {
+		cur.Next = list2
+	}
+	if list2 == nil {
+		cur.Next = list1
+	}
+	return dump.Next
+}
